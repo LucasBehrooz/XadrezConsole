2b31d959e61fd4787abb077d9b58975f37a7b8e6
Lucas Cândido <lucaslordofwar@gmail.com>
2021-09-11 23:06:51 -0300

    Criação da Classe Posição

--- JogoXadrezConsole/Program.cs
+++ JogoXadrezConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace JogoXadrezConsole
 {
@@ -10,6 +11,10 @@ namespace JogoXadrezConsole
     {
         static void Main(string[] args)
         {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + P);
         }
     }
 }

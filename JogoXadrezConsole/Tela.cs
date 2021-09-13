3b3a5cd9a66613087a5cdf618ade567244d23924
Lucas Candido Araujo <Lucasbehrooz@outlook.com>
2021-09-13 16:15:19 -0300

    Melhorando a implementação do metodo imprimirTabuleiro, colorindo as peças

--- JogoXadrezConsole/Tela.cs
+++ JogoXadrezConsole/Tela.cs
@@ -10,6 +10,7 @@ namespace JogoXadrezConsole
 
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8-i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -18,10 +19,29 @@ namespace JogoXadrezConsole
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                      imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                ConsoleColor aux1 = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(peca);
+                Console.ForegroundColor = aux1;
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
